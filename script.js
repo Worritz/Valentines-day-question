1283706279e8aa5b15164c11bb40fdcb6075ff63
Worritz <moritz-rauscher@web.de>
2026-02-05 21:34:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function selectOption(option) {
     // Check which option was clicked
     if (option === 'yes') {
         // Flash rainbow colors
-        
+            document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
         ;
     } else if (option === 'no') {
